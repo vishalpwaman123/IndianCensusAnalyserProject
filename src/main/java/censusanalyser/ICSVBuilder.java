1d13465ab4eba1df9adbe728dabc47ef8150c716
vishalpwaman <vishalpwaman1997@gmail.com>
2020-05-14 21:21:20 -0900

    Refactored. Code to support List in place of Iterator.

--- src/main/java/censusanalyser/ICSVBuilder.java
+++ src/main/java/censusanalyser/ICSVBuilder.java
@@ -2,8 +2,11 @@ package censusanalyser;
 
 import java.io.Reader;
 import java.util.Iterator;
+import java.util.List;
 
 public interface ICSVBuilder<E> {
     public  Iterator<E> getCSVFileIterator(Reader reader, Class csvClass) throws  CSVBuildException;
+    public List<E> getCSVFileList(Reader reader, Class csvClass) throws  CSVBuildException;
 
-    }
+
+}
--- src/main/java/censusanalyser/OpenCSVBuilder.java
+++ src/main/java/censusanalyser/OpenCSVBuilder.java
@@ -5,19 +5,26 @@ import com.opencsv.bean.CsvToBeanBuilder;
 
 import java.io.Reader;
 import java.util.Iterator;
+import java.util.List;
 
 public class OpenCSVBuilder <E> implements ICSVBuilder{
     public Iterator<E> getCSVFileIterator(Reader reader, Class csvClass) throws CSVBuildException {
+        return this.getCSVBean(reader,csvClass).iterator();
+    }
 
+    @Override
+    public List getCSVFileList(Reader reader, Class csvClass) throws CSVBuildException {
+            return this.getCSVBean(reader,csvClass).parse();
+           }
+
+    private CsvToBean<E> getCSVBean(Reader reader, Class csvClass) throws CSVBuildException {
         try {
             CsvToBeanBuilder<E> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
             csvToBeanBuilder.withType(csvClass);
             csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
-            CsvToBean<E> csvToBean = csvToBeanBuilder.build();
-            return csvToBean.iterator();
-        } catch (IllegalStateException e) {
+            return csvToBeanBuilder.build();
+            } catch (IllegalStateException e) {
             throw new CSVBuildException(e.getMessage(),CSVBuildException.ExceptionType.UNABLE_TO_PARSE);
         }
-
     }
 }
